c555e0f739af8fe2ad617506b6d5799d86ad5566
Anthony Liu <liu916@gmail.com>
2017-01-26 23:12:59 -0500

    add requester servicer code

--- src/Main.hs
+++ src/Main.hs
@@ -3,8 +3,15 @@ module Main where
 import Control.Concurrent.STM
 import Control.Concurrent
 import qualified Data.Sequence as S
+import Control.Monad (unless)
+import System.Environment (getArgs)
+import System.IO (readFile)
 
-data Request = Request Int (TVar Bool)
+data Request = Request
+  { _disk :: Int
+  , _requester :: Int
+  , _issued :: (TVar Bool)
+  }
 
 data ServeState = ServeState
   { _activeThreads :: Int
@@ -15,27 +22,59 @@ data ServeState = ServeState
 
 data Servicer = Servicer Int (TVar ServeState)
 
-newRequest :: Int -> IO Request
-newRequest n = atomically $ do
+newRequest :: Int -> Int -> IO Request
+newRequest n r = atomically $ do
   popped <- newTVar False
-  return $ Request n popped
+  return $ Request n r popped
 
 newServicer :: Int -> Int -> IO Servicer
 newServicer n threads = atomically $ do
   st <- newTVar $ ServeState threads 0 0 S.empty
   return $ Servicer n st
 
-issueRequest :: Int -> Servicer -> IO ()
-issueRequest n (Servicer cap tv)= do
-  req@(Request disk tvpopped) <- newRequest n
+issueRequest :: Int -> Int -> Servicer -> IO ()
+issueRequest n r (Servicer cap tv)= do
+  req@(Request disk requester tvpopped) <- newRequest n r
   atomically $ do
     ServeState active current last queue <- readTVar tv
     check $ current < cap
     writeTVar tv $ ServeState active (current+1) last (queue S.|> req)
+  putStrLn $ "requester " ++ show r ++ " track " ++ show n
   atomically $ do
     popped <- readTVar tvpopped
     check popped
 
+removeRequester :: Servicer -> IO ()
+removeRequester (Servicer cap tv) = do
+  atomically $ do
+    ServeState active current last queue <- readTVar tv
+    writeTVar tv $ ServeState (active-1) current last queue
+
+serviceRequest :: Servicer -> IO ()
+serviceRequest (Servicer cap tv) = do
+  (track,req) <- atomically $ do
+    ServeState active current last queue <- readTVar tv
+    check $ (current >= cap) || (current >= active)
+    let go a (b,min) = if (abs (_disk a - _disk b) < min)
+                          then (a, abs (_disk a - _disk b))
+                          else (b, min)
+        Request t r iss = fst $ foldr go (S.index queue 0, maxBound :: Int) queue
+    writeTVar iss False
+    return (t,r)
+  putStrLn $ "service requester " ++ show req ++ " track " ++ show track
+
+runServicer :: Servicer -> IO ()
+runServicer s@(Servicer cap tv) = do
+  active <- atomically $ fmap _activeThreads $ readTVar tv
+  unless (active == 0) $ do
+    serviceRequest s
+    runServicer s
+
+runRequester :: String -> Int -> Servicer -> IO ()
+runRequester f req_numb server = do
+  disk_numbs <- fmap (map read . lines) $ readFile f
+  mapM_ (\n -> issueRequest n req_numb server) disk_numbs
+  removeRequester server
 
 main :: IO ()
 main = do
